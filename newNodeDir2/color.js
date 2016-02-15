10cfecad3877f6a3a3ba98e5c5c991be842632fe
Student <student@RU-005.local>
2016-02-15 16:21:45 -0700

    nothing much

--- newNodeDir2/color.js
+++ newNodeDir2/color.js
@@ -1,7 +1,16 @@
 var request = require('request')
 
+var colorArr = request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function (error, response, body) {
+  JSON.parse(body)
+})
 
+var color = process.argv[2]
+var colorSearch = function(color) {
+	for(var i = 0; i < colorArr.length; i++){
+		if(color.toLowerCase() === colorArr[i].toLowerCase()){
+			console.log(colorArr[i])
+		}
+	}
+}
 
-var obj = JSON.parse(request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json'))
-
-console.log(obj)
+colorSearch()
