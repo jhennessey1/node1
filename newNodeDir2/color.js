08b65c5a97a78a0c6c9f7a5c06c1a3da1f5f19f1
Student <student@RU-005.local>
2016-02-15 16:43:45 -0700

    bonus2 complete

--- newNodeDir2/color.js
+++ newNodeDir2/color.js
@@ -1,16 +1,16 @@
 var request = require('request')
 
-var colorArr = request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function (error, response, body) {
-  JSON.parse(body)
+request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var colorArr = JSON.parse(body)
+
+    var color = process.argv[2]
+
+    for(var i = 0; i < colorArr.length; i++) {
+    	if(colorArr[i].name.toLowerCase() === color.toLowerCase().split(' ').join('')) {
+    		console.log(colorArr[i].rgb.r, colorArr[i].rgb.g, colorArr[i].rgb.b)
+    	}
+    } 
+  }
 })
 
-var color = process.argv[2]
-var colorSearch = function(color) {
-	for(var i = 0; i < colorArr.length; i++){
-		if(color.toLowerCase() === colorArr[i].toLowerCase()){
-			console.log(colorArr[i])
-		}
-	}
-}
-
-colorSearch()
